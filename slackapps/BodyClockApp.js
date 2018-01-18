f0824c83657b1730e015279b2b512e2428a8f1e8
TomHumphrey150 <TomHumphrey@Toms-MacBook-Air-8.local>
2018-01-18 14:54:47 -0800

    Further bodyclock work

--- slackapps/BodyClockApp.js
+++ slackapps/BodyClockApp.js
@@ -1,3 +1,5 @@
+const moment = require('moment-timezone');
+
 const db = require('../database');
 const socketHelpers = require('../server/webSocket.js');
 
@@ -9,13 +11,27 @@ const response = (code, message, method, data) =>
     data,
   });
 
+const convert24HourClockToCircadian = (timeString) => {
+  let timeStringArray = timeWhereTheyAre.split(':');
+    let hour = timeStringArray[0]
+}
+
 const displayWorkspaceBodyClocks = async (messageText, username, workspaceId, ws, wss) => {
 
   let timezonesStringArray = ['These are the timezones of people in here:'];
 
   // get the timezones of everyone in the workspace
   let timezones = await db.getAllTimezonesForWorkspace();
-  timezones.forEach(dbResult => timezonesStringArray.push(`(${dbResult.username}, ${dbResult.current_timezone})`));
+
+  timezones.forEach((dbResult) => {
+    const timeWhereTheyAre = moment.tz(dbResult.current_timezone).format('HH:mm');
+    console.log('Time where they are: ', timeWhereTheyAre);
+    console.log(typeof timeWhereTheyAre);
+
+    
+
+    timezonesStringArray.push(`(${dbResult.username}, ${timeWhereTheyAre}, ${dbResult.current_timezone})`);
+  });
 
   const timezoneReplyString = timezonesStringArray.join(' ');
 
